43bc8aadd0e9bc5370a92c6422cae350b8d2d714
rakassar <115995081+rakassar@users.noreply.github.com>
2022-11-11 12:30:14 +0530

    Update CaseJourneyPageObject.java

--- FramworkDevelopment/crm/mhc/pages/objects/CaseJourneyPageObject.java
+++ FramworkDevelopment/crm/mhc/pages/objects/CaseJourneyPageObject.java
@@ -1,5 +1,6 @@
 package crm.mhc.pages.objects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -17,8 +18,24 @@ public CaseJourneyPageObject (WebDriver driver)
 //***********End*************Fill the Info for  Indivdual process*****************************-------------//
 
 
-@FindBy(xpath="//*[@id=\"2\"]")
+
+
+@FindBy(className="firstchar")
 public static WebElement clk_IndividualCaseJorney;
+
+@FindBy(xpath="//input[@name='CASE_SUBCATEGORY1']")
+public static WebElement fillSSC;
+
+@FindBy(xpath="//a[@data-autoid='FlowNext']") 
+public static WebElement clk_NextOnCasePage;
+
+@FindBy(css="a[data-autoid='FlowNext']")
+public static WebElement clk_casesaveandproceed ;
+
 }
 
-
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
