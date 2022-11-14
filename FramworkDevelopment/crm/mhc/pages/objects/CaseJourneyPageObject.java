136fc8510ab4f98ec201bc683dd16ad6a5ab4133
deepakvermaesolutions@gmail.com <deepakvermaesolutions@gmail.com>
2022-11-14 12:54:39 +0530

    done

--- FramworkDevelopment/crm/mhc/pages/objects/CaseJourneyPageObject.java
+++ FramworkDevelopment/crm/mhc/pages/objects/CaseJourneyPageObject.java
@@ -1,5 +1,7 @@
 package crm.mhc.pages.objects;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -26,10 +28,20 @@ public static WebElement clk_IndividualCaseJorney;
 @FindBy(xpath="//input[@name='CASE_SUBCATEGORY1']")
 public static WebElement fillSSC;
 
+@FindBy(xpath="//a[@data-autoid='CASE_PRDHOLDINGNUMBER_srch']")
+public static WebElement fillcardproduct;
+
+@FindBy(xpath="//div[contains(@data-autoid,'Name')]")
+public static List<WebElement> fillcreditcardproduct;
+
 @FindBy(xpath="//a[@data-autoid='FlowNext']") 
 public static WebElement clk_NextOnCasePage;
 
 @FindBy(css="a[data-autoid='FlowNext']")
 public static WebElement clk_casesaveandproceed ;
+
+
+
+
 
 }
