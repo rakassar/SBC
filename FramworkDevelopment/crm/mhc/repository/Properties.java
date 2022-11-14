6c705c28353f7e362db6d19057adb0fe44ab3c9e
rakassar <115995081+rakassar@users.noreply.github.com>
2022-11-14 18:09:10 +0530

    Update Properties.java
    update

--- FramworkDevelopment/crm/mhc/repository/Properties.java
+++ FramworkDevelopment/crm/mhc/repository/Properties.java
@@ -1,8 +1,8 @@
 package crm.mhc.repository;
 
-        public interface Properties {
-        String ChromeDriverPath= "D:\\Rizwan\\Automation\\lib\\chromedriver.exe"; 
+public interface Properties {
+        String ChromeDriverPath= "D:\\Deepak\\Automation\\Jars\\ChromeDriver\\chromedriver.exe"; 
 		String ChromeWebDriver = "webdriver.chrome.driver";
-		String URL= "https://sbcdev.crmnext.com/sag7/app/login/login";
+		String URL= "https://sbcdev.crmnext.com/accg7/app/login/login";
 	    
 		}
